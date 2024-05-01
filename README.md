101d5c13dbc7b5689fb3159552243d4cd12c1dfd
Gabriel Andrade <45973697+Gabriel5934@users.noreply.github.com>
2024-04-30 22:08:53 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# React Firebase Template
+
+<img src="https://github.com/Gabriel5934/react-firebase/assets/45973697/ffd95672-fcf9-406f-9938-dd10b40603b9" alt="drawing" height="200"/>
+
+- React
+- Typescript
+- Firebase
+- TanStack Router
+- Tailwind
+- Heroicons
---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import "./index.css";
+import { initializeApp } from "firebase/app";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// TODO Add firebase config
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
+  connectFirestoreEmulator(db, "127.0.0.1", 8080);
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+}
+
+const rootElement = document.getElementById("app")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+}
